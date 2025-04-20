c571fb3ce81c2c0b2a02a5d0f0880c564fa202c5
ShiminJin <s.jin.11@student.scu.edu.au>
2025-04-21 00:53:29 +0800

    修复图标

--- ionic-app/src/app/app.component.ts
+++ ionic-app/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons'
+import { 
+  lockClosed, informationCircle, warning, 
+  shieldCheckmark, checkmarkCircle, alertCircle 
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +13,14 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet]
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    addIcons({ 
+      'lock-closed': lockClosed,
+      'information-circle': informationCircle,
+      'warning': warning,
+      'shield-checkmark': shieldCheckmark,
+      'checkmark-circle': checkmarkCircle,
+      'alert-circle': alertCircle
+    });
+  }
 }
